88d9e7a2eeb65dd57cdf9b161d0bf39cd318ebaa
erkle64 <erkle64@gmail.com>
2022-06-01 16:17:11 +1200

    Initial source code upload

---
+++ Tweakificator/Tweakificator/BepInExLoader.cs
@@ -0,0 +1,105 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using UnhollowerRuntimeLib;
+using HarmonyLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using System.IO;
+
+namespace Tweakificator
+{
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public class BepInExLoader : BepInEx.IL2CPP.BasePlugin
+    {
+        public const string
+            MODNAME = "Tweakificator",
+            AUTHOR = "erkle64",
+            GUID = "com." + AUTHOR + "." + MODNAME,
+            VERSION = "1.0.0";
+
+        public static BepInEx.Logging.ManualLogSource log;
+
+        public static string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public static string jsonFolder = Path.Combine(assemblyFolder, "Tweakificator");
+        public static string recipeFolder = Path.Combine(jsonFolder, "Recipes");
+        public static string itemsFolder = Path.Combine(jsonFolder, "Items");
+        public static string tweaksFolder = Path.Combine(assemblyFolder, "..\\tweaks");
+
+        public static bool firstRun = false;
+
+        public static JObject patchData = null;
+        public static JObject patchDataItemChanges = null;
+
+        public BepInExLoader()
+        {
+            log = Log;
+        }
+
+        public override void Load()
+        {
+            if (!Directory.Exists(jsonFolder))
+            {
+                Directory.CreateDirectory(jsonFolder);
+                firstRun = true;
+            }
+
+            if (!Directory.Exists(recipeFolder)) Directory.CreateDirectory(recipeFolder);
+            if (!Directory.Exists(itemsFolder)) Directory.CreateDirectory(itemsFolder);
+            if (!Directory.Exists(tweaksFolder)) Directory.CreateDirectory(tweaksFolder);
+
+            foreach(var path in Directory.GetFiles(tweaksFolder, "*.json"))
+            {
+                var patch = JObject.Parse(File.ReadAllText(path));
+                if(patchData == null)
+                {
+                    log.LogMessage(string.Format("Loading patch {0}", Path.GetFileName(path)));
+                    patchData = patch;
+                }
+                else
+                {
+                    log.LogMessage(string.Format("Merging patch {0}", Path.GetFileName(path)));
+                    patchData.Merge(patch);
+                }
+            }
+
+            if(patchData != null)
+            {
+                patchDataItemChanges = patchData["changes"]["items"] as JObject;
+            }
+
+            log.LogMessage("Registering PluginComponent in Il2Cpp");
+
+            try
+            {
+                ClassInjector.RegisterTypeInIl2Cpp<PluginComponent>();
+
+                var go = new GameObject("Erkle64_Tweakificator_PluginObject");
+                go.AddComponent<PluginComponent>();
+                Object.DontDestroyOnLoad(go);
+            }
+            catch
+            {
+                log.LogError("FAILED to Register Il2Cpp Type: PluginComponent!");
+            }
+
+            try
+            {
+                var harmony = new Harmony(GUID);
+
+                //var original = AccessTools.Method(typeof(NativeWrapper), "templateManager_registerItemTemplate");
+                //var pre = AccessTools.Method(typeof(PluginComponent), "templateManager_registerItemTemplate");
+                //harmony.Patch(original, prefix: new HarmonyMethod(pre));
+
+                var original = AccessTools.Method(typeof(ItemTemplate), "onLoadPostprocess");
+                var post = AccessTools.Method(typeof(PluginComponent), "onLoadItemTemplatePostProcess");
+                harmony.Patch(original, postfix: new HarmonyMethod(post));
+            }
+            catch
+            {
+                log.LogError("Harmony - FAILED to Apply Patch's!");
+            }
+        }
+    }
+}
---
+++ Tweakificator/Tweakificator/PluginComponent.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using UnhollowerBaseLib;
+using HarmonyLib;
+using UnityEngine;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Tweakificator
+{
+    public class PluginComponent : MonoBehaviour
+    {
+        public PluginComponent (IntPtr ptr) : base(ptr)
+        {
+        }
+
+        //[HarmonyPrefix]
+        //public static void templateManager_registerItemTemplate(
+        //    ulong id,
+        //    string identifier,
+        //    ulong modId,
+        //    string modIdentifier,
+        //    string name,
+        //    string nameLocalized,
+        //    ref uint stackSize,
+        //    uint toggleableModeType,
+        //    IOBool skipForRunningIdxGeneration,
+        //    int runningIdx_all,
+        //    long burnable_fuelValueKJ_fpm,
+        //    ulong burnable_residualItemTemplateId,
+        //    uint burnable_residualItemTemplate_count,
+        //    Vector3 meshScale,
+        //    uint minecartMaterialIdx)
+        //{
+        public static void onLoadItemTemplatePostProcess(ItemTemplate __instance)
+        {
+            var path = Path.Combine(BepInExLoader.itemsFolder, __instance.identifier+".json");
+            if(!File.Exists(path))
+            {
+                var itemDump = new ItemDump();
+                itemDump.name = __instance.name;
+                itemDump.stackSize = __instance.stackSize;
+                itemDump.burnable_fuelValueKJ_fpm = __instance.burnable_fuelValueKJ_fpm;
+                itemDump.burnable_residualItemTemplate_str = __instance.burnable_residualItemTemplate_str;
+                itemDump.burnable_residualItemTemplate_count = __instance.burnable_residualItemTemplate_count;
+                itemDump.explosionRadius = __instance.explosionRadius;
+                itemDump.miningRange = __instance.miningRange;
+                itemDump.miningTimeReductionInSec = __instance.miningTimeReductionInSec;
+                File.WriteAllText(path, JsonConvert.SerializeObject(itemDump, Formatting.Indented));
+            }
+
+            if(BepInExLoader.patchDataItemChanges != null && BepInExLoader.patchDataItemChanges.ContainsKey(__instance.identifier))
+            {
+                BepInExLoader.log.LogMessage(string.Format("Patching item {0}", __instance.identifier));
+                var changes = BepInExLoader.patchDataItemChanges[__instance.identifier] as JObject;
+                if (changes.ContainsKey("name")) __instance.name = changes.Value<string>("name");
+                if (changes.ContainsKey("stackSize")) __instance.stackSize = changes.Value<uint>("stackSize");
+                if (changes.ContainsKey("burnable_fuelValueKJ_fpm")) __instance.burnable_fuelValueKJ_fpm = changes.Value<long>("burnable_fuelValueKJ_fpm");
+                if (changes.ContainsKey("burnable_residualItemTemplate_str")) __instance.burnable_residualItemTemplate_str = changes.Value<string>("burnable_residualItemTemplate_str");
+                if (changes.ContainsKey("burnable_residualItemTemplate_count")) __instance.burnable_residualItemTemplate_count = changes.Value<uint>("burnable_residualItemTemplate_count");
+                if (changes.ContainsKey("explosionRadius")) __instance.explosionRadius = changes.Value<int>("explosionRadius");
+                if (changes.ContainsKey("miningRange")) __instance.miningRange = changes.Value<float>("miningRange");
+                if (changes.ContainsKey("miningTimeReductionInSec")) __instance.miningTimeReductionInSec = changes.Value<float>("miningTimeReductionInSec");
+            }
+
+            //BepInExLoader.log.LogMessage(string.Format("onLoadItemTemplate: {0} {1} {2} {3} {4}", __instance.name, __instance.identifier, __instance.id, __instance.includeInBuild, __instance.isHiddenItem));
+            //BepInExLoader.log.LogMessage("onLoadItemTemplate");
+            //BepInExLoader.log.LogMessage(string.Format(" - name: {0}", __instance.name));
+            //BepInExLoader.log.LogMessage(string.Format(" - stackSize: {0}", __instance.stackSize));
+            //BepInExLoader.log.LogMessage(string.Format(" - burnable_fuelValueKJ_fpm: {0}", __instance.burnable_fuelValueKJ_fpm));
+            //BepInExLoader.log.LogMessage(string.Format(" - burnable_residualItemTemplateId: {0}", __instance.burnable_residualItemTemplate_str));
+            //BepInExLoader.log.LogMessage(string.Format(" - burnable_residualItemTemplate_count: {0}", __instance.burnable_residualItemTemplate_count));
+            //BepInExLoader.log.LogMessage(string.Format(" - explosionRadius: {0}", __instance.explosionRadius));
+            //BepInExLoader.log.LogMessage(string.Format(" - miningRange: {0}", __instance.miningRange));
+            //BepInExLoader.log.LogMessage(string.Format(" - miningTimeReductionInSec: {0}", __instance.miningTimeReductionInSec));
+        }
+
+        private struct ItemDump
+        {
+            public string name;
+            public uint stackSize;
+            public long burnable_fuelValueKJ_fpm;
+            public string burnable_residualItemTemplate_str;
+            public uint burnable_residualItemTemplate_count;
+            public int explosionRadius;
+            public float miningRange;
+            public float miningTimeReductionInSec;
+        }
+    }
+}
